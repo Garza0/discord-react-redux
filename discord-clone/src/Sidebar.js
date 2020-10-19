d49768db74ff0d6939e91ac979a9f56ab7096551
Garza0 <konovalovandrey7@gmail.com>
2020-10-19 21:31:11 +0300

    init: start of the app

---
+++ discord-clone/src/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Sidebar.css'
+
+
+function Sidebar() {
+	return (
+		<div className="sidebar">
+			<h2>I am a sidebar</h2>
+
+			<div className="sidebar__top">
+				<h3>Server Name</h3>
+			</div>
+		</div>
+	)
+}
+
+export default Sidebar
