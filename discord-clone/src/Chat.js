cb5d03d91320ff2e9c5ac6904450fd465ba4c3c1
Garza0 <konovalovandrey7@gmail.com>
2020-10-22 00:11:15 +0300

    feat: add channels

--- discord-clone/src/Chat.js
+++ discord-clone/src/Chat.js
@@ -6,11 +6,19 @@ import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
 import GifIcon from '@material-ui/icons/Gif';
 import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
 import Message from './Message';
+import { useSelector } from 'react-redux';
+import { selectchannelId, selectchannelName } from './features/appSlice';
+import { selectUser } from './features/userSlice';
+
 
 function Chat() {
+	const user = useSelector(selectUser);
+	const channelId = useSelector(selectchannelId);
+	const channelName = useSelector(selectchannelName);
+
 	return (
 		<div className="chat">
-			<ChatHeader />
+			<ChatHeader channelName={channelName} />
 			<div className="chat__messages">
 				<Message />
 			</div>
--- discord-clone/src/ChatHeader.js
+++ discord-clone/src/ChatHeader.js
@@ -7,7 +7,7 @@ import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
 import HelpRoundedIcon from '@material-ui/icons/HelpRounded';
 import SendRoundedIcon from '@material-ui/icons/SendRounded';
 
-function ChatHeader() {
+function ChatHeader({ channelName }) {
 	return (
 		<div className="chatHeader">
 			<div className="chatHeader__left">
@@ -15,7 +15,7 @@ function ChatHeader() {
 					<span className="chatHeader__hash">
 						#
 					</span>
-					Test Channel Name
+					{channelName}
 				</h3>
 			</div>
 			<div className="chatHeader__right">
